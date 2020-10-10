f5602b5d7803a7edcdbd318f36821d2ed8a1cd89
siumhossain <sium.hossain@yahoo.com>
2020-10-10 12:17:00 +0600

    password-reset

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('',views.dashboard,name='dashboard'),
     path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),name='password_change'),
+    path('password_change/done',auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),name='password_change_done'),
 ]
