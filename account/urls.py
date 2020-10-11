4e7c0da95e2b92ee4ee4927b43c93a60195bd193
siumhossain <sium.hossain@yahoo.com>
2020-10-11 22:03:22 +0600

    basic login-logout

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
     path('password_change/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),name='password_change'),
     path('password_change/done',auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),name='password_change_done'),
+    path('register/',views.register,name='register'),
 ]
