03bdae3603f765db022eee579385f5e45085bbe6
siumhossain <sium.hossain@yahoo.com>
2020-10-12 13:18:18 +0600

    profile edit

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('password_change/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),name='password_change'),
     path('password_change/done',auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),name='password_change_done'),
     path('register/',views.register,name='register'),
+    path('edit/',views.edit,name='edit'),
 ]
