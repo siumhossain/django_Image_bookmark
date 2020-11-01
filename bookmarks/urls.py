3f3b96a185b5c07d9cf41f4efdbc30a09855de71
siumhossain <sium.hossain@yahoo.com>
2020-11-01 23:46:16 +0600

    somework

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('account.urls')),
+   
+    path('images/', include('images.urls', namespace='images')),
 ]
 
 if settings.DEBUG:
